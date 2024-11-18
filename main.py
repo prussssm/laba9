992ca8b349a69c1fac067dfc78cdb3101f95bf2e
Masha <klimova_v04@bk.ru>
2024-11-18 09:05:30 +0700

    Итерация 5: Реализация первого задания

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-def sort_arrays(arr1, arr2):
-    arr1_sorted = sorted(arr1, reverse=True)
-    arr2_sorted = sorted(arr2)
-    return arr1_sorted, arr2_sorted
+def task_1(arr1, arr2):
+    arr1_sorted, arr2_sorted = sort_arrays(arr1, arr2)
+    result = []
+    
+    for a, b in zip(arr1_sorted, arr2_sorted):
+        if a == b:
+            result.append(0)
+        else:
+            result.append(a + b)
+
+    return sorted(result)
 
 if __name__ == "__main__":
-    array1 = generate_array(5)
-    array2 = generate_array(5)
-    sorted_arrays = sort_arrays(array1, array2)
-    print("Отсортированные массивы:", sorted_arrays)
+    array1 = input_array()
+    array2 = input_array()
+    result = task_1(array1, array2)
+    print("Результат задачи 1:", result)
