7ca2379ecc2b042db5827c16b99e07a395ffaaec
Masha <klimova_v04@bk.ru>
2024-11-18 09:01:50 +0700

    Итерация 2: Функция ручного ввода массива

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-. 
+def input_array():
+    user_input = input("Введите числа через пробел: ")
+    return list(map(int, user_input.split()))
+
+if __name__ == "__main__":
+    test_array = input_array()
+    print("Введенный массив:", test_array)
