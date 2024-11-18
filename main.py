017233612729c2513165906ff76acb2485026315
Masha <klimova_v04@bk.ru>
2024-11-18 09:04:22 +0700

    Итерация 4: Функция сортировки массива

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def main_menu():
-    print("Выберите задание:")
-    print("1. Сумма двух массивов")
-    print("2. Проверка трех массивов")
-    print("3. Поворот матрицы")
-    print("4. Завершение работы программы")
-    
+def sort_arrays(arr1, arr2):
+    arr1_sorted = sorted(arr1, reverse=True)
+    arr2_sorted = sorted(arr2)
+    return arr1_sorted, arr2_sorted
+
 if __name__ == "__main__":
-    main_menu()
+    array1 = generate_array(5)
+    array2 = generate_array(5)
+    sorted_arrays = sort_arrays(array1, array2)
+    print("Отсортированные массивы:", sorted_arrays)
