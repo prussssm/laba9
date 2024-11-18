be682b7ed90957b80e5949ee4fab45b2c3feed1c
Masha <klimova_v04@bk.ru>
2024-11-18 09:08:11 +0700

    Итерация 6: Реализация второго задания

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-def task_1(arr1, arr2):
-    arr1_sorted, arr2_sorted = sort_arrays(arr1, arr2)
-    result = []
+def task_2(arr1, arr2, arr3):
+    results = []
     
-    for a, b in zip(arr1_sorted, arr2_sorted):
-        if a == b:
-            result.append(0)
-        else:
-            result.append(a + b)
-
-    return sorted(result)
+    for a, b, c in zip(arr1, arr2, arr3):
+        if a + b == c:
+            results.append((a + b + c) ** min(a, b, c))
+    
+    return results
 
 if __name__ == "__main__":
     array1 = input_array()
     array2 = input_array()
-    result = task_1(array1, array2)
-    print("Результат задачи 1:", result)
+    array3 = input_array()
+    result = task_2(array1, array2, array3)
+    print("Результат задачи 2:", result)
